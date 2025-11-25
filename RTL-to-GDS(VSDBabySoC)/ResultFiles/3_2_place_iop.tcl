cde49f480cd607b88f0fd30f9be9bb75d9c03966
Jayessh S.K <s.k.jayessh@gmail.com>
2025-11-25 22:38:36 +0530

    Add files via upload

---
+++ RTL-to-GDS(VSDBabySoC)/ResultFiles/3_2_place_iop.tcl
@@ -0,0 +1,11 @@
+#Edge: BOTTOM
+place_pin -pin_name ENb_CP -layer met2 -location {73.83 0.242} -force_to_die_boundary
+place_pin -pin_name ENb_VCO -layer met2 -location {82.11 0.242} -force_to_die_boundary
+place_pin -pin_name OUT -layer met2 -location {1096.87 0.242} -force_to_die_boundary
+place_pin -pin_name REF -layer met2 -location {89.47 0.242} -force_to_die_boundary
+place_pin -pin_name VCO_IN -layer met2 -location {61.87 0.242} -force_to_die_boundary
+#Edge: RIGHT
+place_pin -pin_name reset -layer met3 -location {1599.6 409.7} -force_to_die_boundary
+#Edge: TOP
+#Edge: LEFT
+place_pin -pin_name VREFH -layer met3 -location {0.4 158.1} -force_to_die_boundary
